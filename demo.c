e224f16bbd024ae291a6485c43c5e3b656e543bd
DM <sinnistra@gmail.com>
2011-10-13 16:23:00 +0200

    Added more functions into demo.c

--- demo.c
+++ demo.c
@@ -2,10 +2,21 @@
 #include <unistd.h>
 #include <math.h>
 
+void function(int n)
+{
+	//if (n==0) return;
+	
+	printf("test");
+	//function(n-1);
+}
+
 int main() {
 	int i = 0, j = 1;
 	for(; i < 3; i++) {
 		j += i;
 		sleep(1);
 	}
+	function(1);
+	function(2);
+	function(3);
 }
