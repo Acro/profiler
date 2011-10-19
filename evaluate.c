d74d78c1a769f0df611d4421a91b794ed599bb07
DM <sinnistra@gmail.com>
2011-10-19 18:30:34 +0200

    Analyzer is now able to take recursions in concideration, minor code ajustements

--- evaluate.c
+++ evaluate.c
@@ -4,8 +4,8 @@
  Name        : evaluate.c
  Author      :
  Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Copyright   :
+ Description : Output analyzer
  
 ============================================================================
  */
@@ -16,65 +16,92 @@
 
 static FILE *fp_trace;
 struct data_stack{
-		char function[200];
-		//float begin;
-		//float end;
-		int beginSec;
-		int beginUSec;
-		int endSec;
-		int endUSec;
-		struct data_stack* next;
 
+		char name[200];
+		double beginSec;
+		double totalSec;
+		int recursion;
+		struct data_stack* next;
 };
-struct data_stack* put(struct data_stack *stack, struct data_stack *new){
+
+struct data_stack* functionEntered(struct data_stack *stack, struct 
+data_stack *new)
+{
+
     if (new == NULL) return stack;
-	if(stack==NULL){
-		stack=new;
+
+	if (stack == NULL)
+	{
+		stack = new;
 		stack->next = NULL;
+		stack->recursion = 1;
+		stack->totalSec = 0;
 	}
-	else{
-		struct data_stack *pos=stack;
-		while(pos->next!=NULL){
-			pos=pos->next;
+	else
+	{
+		struct data_stack *pos = stack;
+
+        //If function is present on the stack, it will modify it
+		while(pos != NULL)
+		{
+		    if (strcmp(pos->name, new->name) == 0)
+		    {
+                    if (pos->recursion > 0) pos->recursion += 1;
+                    else
+                    {
+                        pos->beginSec = new->beginSec;
+                        pos->recursion = 1;
+                    }
+                    free(new);
+                    return stack;
+		    }
+			pos = pos->next;
 		}
-		pos->next=new;
-		new->next=NULL;
+
+        //Else add function to the end
+		pos = stack;
+
+		while(pos->next != NULL)
+		{
+			pos = pos->next;
+		}
+
+		pos->next = new;
+		new->next = NULL;
+		new->totalSec = 0;
+		new->recursion = 1;
 	}
 	return stack;
 }
-struct data_stack* getAndRem(struct data_stack *stack){
-	if(stack!=NULL){
-		struct data_stack *ret;
-		if(stack->next!=NULL){
-			struct data_stack *pos=stack;
-			while(pos->next->next != NULL){
-				pos=pos->next;
-			}
-			ret=pos->next;
-			pos->next=NULL;
-			return ret;
-		}else {
-			ret=stack;
-			stack=NULL;
-			return ret;
-		}
-	}
-	return NULL;
+
+void functionExited(struct data_stack *stack, struct data_stack *new)
+{
+    struct data_stack *pos = stack;
+
+	while(pos != NULL)
+	{
+	    if (strcmp(pos->name, new->name) == 0)
+	    {
+            if (pos->recursion > 1) pos->recursion -= 1;
+            else
+            {
+                pos->totalSec += new->beginSec - pos->beginSec;
+                pos->recursion = 0;
+            }
+            free(new);
+            return;
+        }
+        pos = pos->next;
+    }
+    free(new);
 }
 
-struct data_stack* get(struct data_stack *stack){
-	if(stack==NULL)return NULL;
-	struct data_stack *pos=stack;
-	while(pos->next!=NULL){
-		pos=pos->next;
-	}
-	return pos;
-}
 
 void printStack(struct data_stack *stack){
 	struct data_stack *toDel;
-	while(stack!=NULL){
-		printf("Function %s %ds %dms\n", stack->function,(stack->endSec-stack->beginSec),(stack->endUSec-stack->beginUSec));
+	while(stack!=NULL)
+	{
+		printf("Function %s %fs\n", stack->name, stack->totalSec);
 		toDel=stack;
 		stack=stack->next;
 		free(toDel);
@@ -87,36 +114,35 @@ int main(void) {
 
 	char line[251]={0};
 
-	struct data_stack *inProgressStack=NULL;
-	struct data_stack *finalStack=NULL;
-	char empty[200];
+	struct data_stack *functions=NULL;
+
 	while(NULL!=fgets(line,250,fp_trace)){
-		char function[200]={0};
+
+		char name[200]={0};
 		int sec;
 		int usec;
 		char type;
-		sscanf(line,"%c %*s %s %d %d", &type, function, &sec, &usec);
+
+		sscanf(line,"%c %*s %s %d %d", &type, name, &sec, &usec);
 		struct data_stack *data;
-		if(type=='e'){
-			data=malloc(sizeof(struct data_stack));
-			strcpy(data->function,function);
-			data->beginSec=sec;
-			data->beginUSec=usec;
-			inProgressStack=put(inProgressStack,data);
+
+		data=malloc(sizeof(struct data_stack));
+		strcpy(data->name, name);
+        data->beginSec=sec + (double)(usec)/1000000;
+
+		if(type=='e')
+		{
+			functions=functionEntered(functions,data);
 		}
 		else if(type=='x')
 		{
-			//if(!strcmp(function,data->function)) return 2;
-			data=getAndRem(inProgressStack);
-			data->endSec=sec;
-			data->endUSec=usec;
-			finalStack=put(finalStack,data);
-		}else return 1;
-		//sscanf(line,"");
+			functionExited(functions,data);
+		}
+		else return 1;
 	}
-	printStack(finalStack);
-	//getc();
+	printStack(functions);
 	fclose(fp_trace);
+
 	return EXIT_SUCCESS;
 }
 
