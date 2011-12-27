9172e2ddb347cc9d88a98f231e0dc5d67e931dd3
Acro <acro@ubuntu.(none)>
2011-12-27 20:22:31 +0100

    Specific function tracing added Full tracing heavily upgraded Bash script for compiling upgraded

---
+++ trace_full.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/time.h>
+
+struct data {
+	unsigned long int sec;
+	unsigned int usec;
+	void* func;
+	void* caller;
+	unsigned short state;
+	struct data* next;
+};
+static struct data* start = NULL;
+static struct data* last = NULL;
+static struct data* out;
+static struct timeval current_time;
+
+/*
+void 
+__attribute__ ((constructor))
+trace_begin (void) {
+
+}
+*/
+
+void
+__attribute__ ((destructor))
+trace_end (void) {
+	FILE* test = fopen("out.txt", "w");
+	if(test != NULL) {
+		struct data* stack = start;
+		while(stack != NULL) {
+			if(!stack->state) {
+				fprintf(test, "e %p %p %lu %d\n", stack->func, stack->caller, stack->sec, stack->usec );
+			} else {
+				fprintf(test, "x %p %p %lu %d\n", stack->func, stack->caller, stack->sec, stack->usec );
+			}
+			struct data* prev = stack;
+			stack = stack->next;
+			free(prev);
+		}
+		fclose(test);
+	}
+}
+
+void
+__cyg_profile_func_enter (void *func, void *caller) {
+	out = malloc(sizeof(struct data));
+	out->func = func;
+	out->caller = caller;
+
+	gettimeofday( &current_time, NULL);
+
+	out->sec = current_time.tv_sec;
+	out->usec = current_time.tv_usec;
+	out->state = 0;
+	out->next = NULL;
+
+	if(start == NULL) {
+		start = out;
+		last = out;
+	} else {
+		last->next = out;
+		last = out;
+	}
+}
+
+void
+__cyg_profile_func_exit (void *func, void *caller) {
+	gettimeofday( &current_time, NULL);
+
+	out = malloc(sizeof(struct data));
+	out->func = func;
+	out->caller = caller;
+	out->sec = current_time.tv_sec;
+	out->usec = current_time.tv_usec;
+	out->state = 1;
+	out->next = NULL;
+
+	if(start == NULL) {
+		start = out;
+		last = out;
+	} else {
+		last->next = out;
+		last = out;
+	}
+}
---
+++ trace_specific.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/time.h>
+
+static char target_function[256];
+
+struct data {
+	long unsigned int sec;
+	long unsigned int usec;
+	void* func;
+	void* caller;
+	int state;
+	struct data* next;
+};
+
+static struct data* start = NULL;
+static struct data* last = NULL;
+static struct data* out;
+static void* profiled_func = NULL;
+static struct timeval current_time;
+static char buffer[256];
+
+void addr2name(char* name) {
+	FILE *fp;
+	char cmd[250];
+	
+	strcpy(cmd, "addr2line -f -e main ");
+	strcat(cmd, name);
+	strcat(cmd," | head -1");
+
+	fp = popen( cmd , "r");
+	fgets(name, 200, fp);
+	pclose(fp);
+}
+
+void 
+__attribute__ ((constructor))
+trace_begin (void) {
+	FILE* f = fopen("func.txt", "r"); // reads file created during compilation, if emtpy, no specific tracing should be done
+	if(f != NULL) {
+		fgets(target_function, 255, f);
+		target_function[strlen(target_function)-1] = '\0';
+		fclose(f);
+	}
+}
+
+void
+__attribute__ ((destructor))
+trace_end (void) {
+	FILE* f = fopen("out.txt", "w");
+	if(f != NULL) {
+		struct data* stack = start;
+		while(stack != NULL) {
+			if(stack->state == 0) {
+				fprintf(f, "e %p %p %lu %lu\n", stack->func, stack->caller, stack->sec, stack->usec );
+			} else {
+				fprintf(f, "x %p %p %lu %lu\n", stack->func, stack->caller, stack->sec, stack->usec );
+			}
+			struct data* prev = stack;
+			stack = stack->next;
+			free(prev);
+		}
+		fclose(f);
+	}
+}
+
+void
+__cyg_profile_func_enter (void *func, void *caller) {
+	// gettimeofday( &current_time, NULL);
+
+	if(profiled_func == NULL) {
+		sprintf(buffer, "%p", func);
+		addr2name(buffer);
+		buffer[strlen(buffer)-1] = '\0';
+
+		if(strcmp(buffer, target_function) == 0) {
+			profiled_func = func;
+		} else {
+			return;
+		}
+	}
+
+	if(profiled_func == func) {
+		out = malloc(sizeof(struct data));
+		out->func = profiled_func;
+		out->caller = caller;
+
+		gettimeofday( &current_time, NULL);
+
+		out->sec = current_time.tv_sec;
+		out->usec = current_time.tv_usec;
+		out->state = 0;
+		out->next = NULL;
+
+		if(start == NULL) {
+			start = out;
+			last = out;
+		} else {
+			last->next = out;
+			last = out;
+		}
+	}
+}
+
+void
+__cyg_profile_func_exit (void *func, void *caller) {
+	if(profiled_func == func) {
+		gettimeofday( &current_time, NULL);
+
+		out = malloc(sizeof(struct data));
+		out->func = profiled_func;
+		out->caller = caller;
+		out->sec = current_time.tv_sec;
+		out->usec = current_time.tv_usec;
+		out->state = 1;
+		out->next = NULL;
+
+		if(start == NULL) {
+			start = out;
+			last = out;
+		} else {
+			last->next = out;
+			last = out;
+		}
+	}
+}
